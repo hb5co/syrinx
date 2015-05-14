e8220efee4ee633fba6658fd9f14bb5fbfba70fc
Mike Barkas <mike@barkas.com>
2015-05-13 23:44:38 -0400

    Added sort field to startup fixture.

--- server/fixtures.hosts.js
+++ server/fixtures.hosts.js
@@ -4,12 +4,14 @@ hostFixtures = [
     hostname: 'drupal7.site',
     type: 'drupal',
     version: 7,
+    sort: 1,
     last_check: null
   },
   {
     hostname: 'drupal6.site',
     type: 'drupal',
     version: 6,
+    sort: 2,
     last_check: null
   }
 ];
