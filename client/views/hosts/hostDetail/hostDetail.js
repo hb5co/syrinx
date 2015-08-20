215a112393df3e94eb2c62deec97e167a4e296f2
Lauren Ficco <LaurenF@knowclassic.com>
2015-08-20 15:03:27 -0400

    Adding support to fetch a website to determine if it "exists" or not

--- client/views/hosts/hostDetail/hostDetail.js
+++ client/views/hosts/hostDetail/hostDetail.js
@@ -21,6 +21,31 @@ Template.HostDetail.events({
           hostUpdated: new Date
         }
       });
+
+      // Update the status of the host once a response has been determined
+      // asynchronously.
+      Meteor.call("updateHostStatus", hostname, function(err, response) {
+
+        // If there was an error or if there was no response, then consider this
+        // site to be offline.
+        if (err || response != "ok") {
+          Hosts.update({_id: id}, {
+            $set: {
+              status: "remove"
+            }
+          });
+        }
+
+        // If a response was successful, then consider this site to be online.
+        else {
+          Hosts.update({_id: id}, {
+            $set: {
+              status: "ok"
+            }
+          });
+        }
+      });
+
       // After update, go to detail view page.
       Router.go('host.detail', {_id: id});
     }
